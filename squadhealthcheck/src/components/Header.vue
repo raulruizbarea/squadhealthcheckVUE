4c233cc868447bcafb78adc74e7d471d58e9fb22
Raul <raul.ruiz.barea@gmail.com>
2020-05-11 23:17:09 +0200

    Enhance header

--- squadhealthcheck/src/components/Header.vue
+++ squadhealthcheck/src/components/Header.vue
@@ -1,21 +1,16 @@
 <template>
     <div id="shc-header">
-        <div id="shc-title">
-            <section>
-                <h1>Squad Health Check</h1>
-                <br />
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star-half-o"></span>
-                <p>
-                    Increased awareness of where the squad is at, it’s state, challenges and opportunities.
-                </p>
-                
-            </section>
-            <router-link to="/"><img src="../assets/logo.png" /></router-link>
-        </div>
+        <section>
+            <h1>Squad Health Check</h1>
+            <br />
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star-half-o"></span>
+            <p>Increased awareness of where<br/>the squad is at, it’s state,<br/>challenges and opportunities.</p>
+        </section>
+        <router-link to="/"><img src="../assets/logo.png" /></router-link>
     </div>
 </template>
 
@@ -24,20 +19,36 @@
         color: white;
         text-align: center;
         background-color: #FFA200;
+        border-bottom-left-radius: 50% 15%;
+        border-bottom-right-radius: 50% 15%;
     }
 
     section {
-        height: 14em;
+        height: 16em;
+    }
+
+    section h1 {
+        padding-top: 1.75em;
+    }
+
+    section span {
+        padding-left: 0.5em;
+        font-size: 0.75em;
+    }
+
+    section p {
+        margin-top: 0.25em;
+        line-height: 1em;
     }
 
     img {
-        width: 8em;
+        width: 7em;
         position: absolute;
         margin-left: auto;
         margin-right: auto;
         left: 0;
         right: 0;
         text-align: center;
-        top: 10em;
+        top: 12.5em;
     }
 </style>
