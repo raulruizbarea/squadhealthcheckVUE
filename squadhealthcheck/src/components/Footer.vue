6c5ef51027b9c865f48ec8ddbdc691d947815931
Raul <raul.ruiz.barea@gmail.com>
2020-05-11 23:16:56 +0200

    Enhance footer

--- squadhealthcheck/src/components/Footer.vue
+++ squadhealthcheck/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="shc-footer">
-        <p>By using this application, you agree to our <b>Terms of Service</b> and <b>Privacy Policy</b></p>
+        <p>By using this application, you agree to our<br/><b>Terms of Service</b> and <b>Privacy Policy</b></p>
     </div>
 </template>
 
@@ -9,7 +9,8 @@
         text-align: center;
         flex-shrink: 0;
         /* Prevent Chrome, Opera, and Safari from letting these items shrink to smaller than their content's default minimum size. */
-        padding: 1em 4em 1em 4em;
+        padding: 1em 0 1em 0;
         font-size: 0.8em;
+        line-height: 1em;
     }
 </style>
