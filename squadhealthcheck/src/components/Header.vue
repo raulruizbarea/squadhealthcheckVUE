5149dbe0d244458ee537ed49945623039a821e22
Raul <raul.ruiz.barea@gmail.com>
2020-05-12 14:50:22 +0200

    Added round header

--- squadhealthcheck/src/components/Header.vue
+++ squadhealthcheck/src/components/Header.vue
@@ -1,15 +1,19 @@
 <template>
-    <v-container class="secondary primary--text text-center" fluid>
-        <h1 class="pt-6">Squad Health Check</h1>
-        <v-col class="subtitle-1 text--line-height-s mb-12">
-            <v-rating v-model="rating" size="14" readonly dense half-increments class="pb-2"></v-rating>
-            <span v-html="slogan"></span>
-        </v-col>
-        <router-link to="/"><v-img src="../assets/logo.png" width="7rem" class="shc-img"></v-img></router-link>
-    </v-container>
+  <v-container class="secondary primary--text text-center shc-header" fluid>
+    <h1 class="pt-6">Squad Health Check</h1>
+    <v-col class="subtitle-1 text--line-height-s mb-12">
+      <v-rating v-model="rating" size="14" readonly dense half-increments class="pb-2"></v-rating>
+      <span v-html="slogan"></span>
+    </v-col>
+    <router-link to="/"><v-img src="../assets/logo.png" width="7rem" class="shc-img"></v-img></router-link>
+  </v-container>
 </template>
 
 <style scoped>
+  .shc-header {
+    border-radius: 0 0 100% 100%;
+  }
+
   .shc-img {
     position: absolute;
     margin-left: auto;
@@ -20,6 +24,7 @@
     z-index: 999;
   }
 </style>
+
 <script>
   export default {
     name: 'Header',
