c5d0ffde717449ba2b6e973e6a0375c1d90cfe14
Raul <raul.ruiz.barea@gmail.com>
2020-05-12 18:23:45 +0200

    Clean styles inside create view

--- squadhealthcheck/src/views/Create.vue
+++ squadhealthcheck/src/views/Create.vue
@@ -15,12 +15,6 @@
   </div>
 </template>
 
-<style scoped>
-  .create {
-    flex: 1 0 auto;
-  }
-</style>
-
 <script>
   import {FormWizard, TabContent} from 'vue-form-wizard';
   import 'vue-form-wizard/dist/vue-form-wizard.min.css';
