2a81c9208b1e76fb9cb0d48c6c752afb630010d2
Raul <raul.ruiz.barea@gmail.com>
2020-05-12 11:32:39 +0200

    Vuetify applied to Footer

--- squadhealthcheck/src/components/Footer.vue
+++ squadhealthcheck/src/components/Footer.vue
@@ -1,16 +1,9 @@
 <template>
-    <div id="shc-footer">
-        <p>By using this application, you agree to our<br/><b>Terms of Service</b> and <b>Privacy Policy</b></p>
-    </div>
+    <v-footer absolute padless>
+        <v-col class="caption text-center text--secondary text--line-height-s">
+           By using this application, you agree to our
+           <v-spacer></v-spacer>
+           <strong>Terms of Service</strong> and <strong>Privacy Policy</strong>
+        </v-col>
+    </v-footer>
 </template>
-
-<style scoped>
-    #shc-footer {
-        text-align: center;
-        flex-shrink: 0;
-        /* Prevent Chrome, Opera, and Safari from letting these items shrink to smaller than their content's default minimum size. */
-        padding: 1em 0 1em 0;
-        font-size: 0.8em;
-        line-height: 1em;
-    }
-</style>
