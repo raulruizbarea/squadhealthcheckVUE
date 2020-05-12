f5cc7459838f8c7c54b3ebf4fab15a2ac570d804
Raul <raul.ruiz.barea@gmail.com>
2020-05-12 11:56:06 +0200

    Footer: Add text as a variable

--- squadhealthcheck/src/components/Footer.vue
+++ squadhealthcheck/src/components/Footer.vue
@@ -1,9 +1,15 @@
 <template>
     <v-footer absolute padless>
-        <v-col class="caption text-center text--secondary text--line-height-s">
-           By using this application, you agree to our
-           <v-spacer></v-spacer>
-           <strong>Terms of Service</strong> and <strong>Privacy Policy</strong>
+        <v-col class="caption text-center text--secondary text--line-height-s" v-html="terms">
         </v-col>
     </v-footer>
 </template>
+
+<script>
+    export default {
+        name: 'Footer',
+        data: () => ({
+            terms: 'By using this application, you agree to our<br /><strong>Terms of Service</strong> and <strong>Privacy Policy</strong>',
+        }),
+    }
+</script>
