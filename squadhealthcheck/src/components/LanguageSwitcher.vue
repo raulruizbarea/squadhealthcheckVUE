06671e20a50d2828b810ba90b2122a95ce28b955
Raul <raul.ruiz.barea@gmail.com>
2020-05-13 16:01:00 +0200

    Added Language Switcher component

---
+++ squadhealthcheck/src/components/LanguageSwitcher.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-item-group mandatory>
+        <v-container fluid pa-0>
+            <v-row justify="end">
+                <div v-for="(lang, index) in languages" :key="lang">
+                    <v-item>
+                        <div class="mr-2 svh-language-switcher-pointer" 
+                            :class="{'font-weight-bold': index === activeItem, 'mr-4': index === languages.length-1}"
+                            @click="changeLang(lang, index)">                            
+                            {{ lang.toUpperCase() }}
+                        </div>
+                    </v-item>
+                </div>
+            </v-row>
+        </v-container>
+  </v-item-group>
+</template>
+
+<style scoped>
+    .svh-language-switcher-pointer {
+        cursor: pointer;
+    }
+</style>
+
+<script>
+    export default {
+        name: 'LanguageSwitcher',
+        data: () => ({ 
+            languages: ['en', 'es'],
+            activeItem: 0,
+        }),
+        methods: {
+            changeLang (lang, idx) {
+                this.activeItem = idx;
+                this.$root.$i18n.locale = lang;
+            }
+        }
+    }
+</script>
