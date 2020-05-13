328863d5b8c54049dbf662f1b9e5266fdf8dded8
Raul <raul.ruiz.barea@gmail.com>
2020-05-13 16:00:38 +0200

    Rework

--- squadhealthcheck/src/App.vue
+++ squadhealthcheck/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <v-app>
-    <v-content> 
-      <router-view/>
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid pa-0>
+        <!-- If using vue-router -->
+        <router-view/>
+      </v-container>
     </v-content>
     <shc-footer />
   </v-app>
