f98723e74bc55e276b563c4056e98aeadcce4207
Raul <raul.ruiz.barea@gmail.com>
2020-05-13 14:07:55 +0200

    Add i18n plugin

--- squadhealthcheck/src/main.js
+++ squadhealthcheck/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue';
 import router from './router';
 import './assets/global.css';
 import vuetify from './plugins/vuetify';
+import { i18n } from '@/plugins/i18n';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount('#app');
