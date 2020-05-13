f8829f6210aa8d4e3943edec2bf030933b91b83a
Raul <raul.ruiz.barea@gmail.com>
2020-05-13 19:24:06 +0200

    Add ToolBar component

---
+++ squadhealthcheck/src/components/ToolBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-toolbar color="secondary" dark>
+        <v-btn to="/" icon>
+            <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+        <!--<v-toolbar-title>Title</v-toolbar-title>-->
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: 'ToolBar',
+    }
+</script>
