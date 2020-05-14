7a558767c4a7120a6b09a74d20055928854d5d23
Raul <raul.ruiz.barea@gmail.com>
2020-05-14 22:49:51 +0200

    Add lang in create view

--- squadhealthcheck/src/views/Create.vue
+++ squadhealthcheck/src/views/Create.vue
@@ -5,87 +5,89 @@
       <v-stepper v-model="e6" vertical class="elevation-0">
         <v-stepper-step :complete="e6 > 1" step="1" color="secondary">
           {{ $t('createStepMsg1') }}
-          <small>Next: Awaiting squad team</small>
+          <small>{{$t('next')}}: {{$t('awaitingTeam')}}</small>
         </v-stepper-step>
-
+        
         <v-stepper-content step="1">
-          <v-text-field
-            v-model="squadName"
-            :counter="40"
-            :rules="squadNameRules"
-            v-bind:label="$t('squadName')"
-            required
-            color="secondary"
-          ></v-text-field>
-          <v-subheader class="pl-0">{{ $t('sprintNumber') }}</v-subheader>
-          <span
-            class="subtitle-1"
-            v-text="currentSprint"
-          ></span>
-          <v-slider
-            v-model="currentSprint"
-            color="secondary"
-            track-color="gray"
-            :thumb-label="true"
-            min="1"
-            max="50"
-            class="px-5"
-            :thumb-size="24"
-            hide-details
-          ></v-slider>
-          <v-subheader class="pl-0">{{ $t('squadSize') }}</v-subheader>
-          <span
-            class="subtitle-1"
-            v-text="currentSize"
-          ></span>
-          <v-slider
-            v-model="currentSize"
-            color="secondary"
-            track-color="gray"
-            :thumb-label="true"
-            min="2"
-            max="20"
-            class="px-5"
-            :thumb-size="24"
-            hide-details
-          ></v-slider>
-          <v-layout column align-center>
-            <v-switch 
-              v-model="anonymous" 
-              :label="$t('anonymous')"
+          <v-form ref="form" v-model="step1Valid">
+            <v-text-field
+              v-model="squadName"
+              :counter="40"
+              :rules="squadNameRules"
+              v-bind:label="$t('squadName')"
+              required
               color="secondary"
-              inset>
-            </v-switch>
-          </v-layout>
-          <!--v-btn block to="/create" color="secondary" x-large elevation="10" rounded>{{ $t('create') }}</v-btn-->
-          <v-row justify="end" class="px-4">
-            <v-btn :ripple="false" text @click="$router.go(-1)" class="btnNoEffect">Cancel</v-btn>
-            <v-btn color="secondary" @click="e6 = 2">Continue</v-btn>
-          </v-row>
+            ></v-text-field>
+            <v-subheader class="pl-0">{{ $t('sprintNumber') }}</v-subheader>
+            <span
+              class="subtitle-1"
+              v-text="currentSprint"
+            ></span>
+            <v-slider
+              v-model="currentSprint"
+              color="secondary"
+              track-color="gray"
+              :thumb-label="true"
+              min="1"
+              max="50"
+              class="px-5"
+              :thumb-size="24"
+              hide-details
+            ></v-slider>
+            <v-subheader class="pl-0">{{ $t('squadSize') }}</v-subheader>
+            <span
+              class="subtitle-1"
+              v-text="currentSize"
+            ></span>
+            <v-slider
+              v-model="currentSize"
+              color="secondary"
+              track-color="gray"
+              :thumb-label="true"
+              min="2"
+              max="20"
+              class="px-5"
+              :thumb-size="24"
+              hide-details
+            ></v-slider>
+            <v-layout column align-center>
+              <v-switch 
+                v-model="anonymous" 
+                :label="$t('anonymous')"
+                color="secondary"
+                inset>
+              </v-switch>
+            </v-layout>
+            <!--v-btn block to="/create" color="secondary" x-large elevation="10" rounded>{{ $t('create') }}</v-btn-->
+            <v-row justify="end" class="px-4">
+              <v-btn :ripple="false" text @click="$router.go(-1)" class="btnNoEffect">{{$t('cancel')}}</v-btn>
+              <v-btn :disabled="!step1Valid" color="secondary" @click="e6 = 2">{{$t('continue')}}</v-btn>
+            </v-row>
+          </v-form>
         </v-stepper-content>
 
         <v-stepper-step :complete="e6 > 2" step="2" color="secondary">
           Awaiting squad team
-          <small>Next: Status</small>
+          <small>{{$t('next')}}: {{$t('status')}}</small>
         </v-stepper-step>
 
         <v-stepper-content step="2">
           <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
           <v-row justify="end" class="px-4">
-            <v-btn :ripple="false" text @click="e6 = 1" class="btnNoEffect">Cancel</v-btn>
-            <v-btn color="secondary" @click="e6 = 3">Continue</v-btn>
+            <v-btn :ripple="false" text @click="e6 = 1" class="btnNoEffect">{{$t('cancel')}}</v-btn>
+            <v-btn :disabled="!step1Valid" color="secondary" @click="e6 = 3">{{$t('continue')}}</v-btn>
           </v-row>
         </v-stepper-content>
 
         <v-stepper-step :complete="e6 > 3" step="3" color="secondary">
-          Status
+          {{$t('status')}}
         </v-stepper-step>
 
         <v-stepper-content step="3">
           <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
           <v-row justify="end" class="px-4">
-            <v-btn :ripple="false" text @click="e6 = 2" class="btnNoEffect">Cancel</v-btn>
-            <v-btn color="secondary">Finish</v-btn>
+            <v-btn :ripple="false" text @click="e6 = 2" class="btnNoEffect">{{$t('cancel')}}</v-btn>
+            <v-btn :disabled="!step1Valid" color="secondary">{{$t('finish')}}</v-btn>
           </v-row>
         </v-stepper-content>
       </v-stepper>      
@@ -104,6 +106,7 @@
   
   export default {
     data: () => ({
+      step1Valid: true,
       squadName: '',
       squadNameRules: [
         v => !!v || 'Squad name is required',
@@ -119,6 +122,7 @@
       'tool-bar': ToolBar,
     },
     methods: {
+
     },
   }
 </script>
