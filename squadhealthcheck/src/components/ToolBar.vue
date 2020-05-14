024fedb0490d593c41e2e1a15a08c5d4cc4e5287
Raul <raul.ruiz.barea@gmail.com>
2020-05-14 18:02:46 +0200

    Totally center toolbar

--- squadhealthcheck/src/components/ToolBar.vue
+++ squadhealthcheck/src/components/ToolBar.vue
@@ -1,11 +1,18 @@
 <template>
     <v-toolbar color="secondary" dark elevation="0">
-        <v-btn to="/" icon>
-            <v-icon>mdi-arrow-left</v-icon>
-        </v-btn>
-        <v-spacer></v-spacer>
-        <v-toolbar-title primary>{{ $t('appName') }}</v-toolbar-title>
-        <v-spacer></v-spacer>
+        <v-row align="center">
+            <v-col>
+                <v-btn to="/" icon>
+                    <v-icon>mdi-arrow-left</v-icon>
+                </v-btn>
+            </v-col>
+            <v-col class="d-flex justify-space-around">
+                <v-toolbar-title primary>{{ $t('appName') }}</v-toolbar-title>
+            </v-col>
+            <v-col class="d-flex justify-end">
+      
+            </v-col>
+        </v-row>
     </v-toolbar>
 </template>
 
