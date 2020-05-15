0933bf7cf3449ad3a42ea34569c4dba803c62427
Raul <raul.ruiz.barea@gmail.com>
2020-05-15 18:16:14 +0200

    Add store in main.js

--- squadhealthcheck/src/main.js
+++ squadhealthcheck/src/main.js
@@ -4,11 +4,13 @@ import router from './router';
 import './assets/global.css';
 import vuetify from './plugins/vuetify';
 import { i18n } from '@/plugins/i18n';
+import { store } from './store/store'
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   vuetify,
   i18n,
   render: h => h(App)
