c6683bd54a41104b6f4be2649d60f4117ea8f262
Raul <raul.ruiz.barea@gmail.com>
2020-05-15 19:44:27 +0200

    Added firebase init and config

---
+++ squadhealthcheck/src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAkN9gm76hIB88OhhEuV1SuuAxTdYeavRI",
+    authDomain: "squadhealthcheck-fb8f1.firebaseapp.com",
+    databaseURL: "https://squadhealthcheck-fb8f1.firebaseio.com",
+    projectId: "squadhealthcheck-fb8f1",
+    storageBucket: "squadhealthcheck-fb8f1.appspot.com",
+    messagingSenderId: "30406816004",
+    appId: "1:30406816004:web:7ece02d5ce5e2292832051"
+};
