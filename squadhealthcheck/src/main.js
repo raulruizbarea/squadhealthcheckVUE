7ae20c64faed694b50ea712d0c5d0cefacb731b4
Raul <raul.ruiz.barea@gmail.com>
2020-05-18 00:33:19 +0200

    VueClipboard config

--- squadhealthcheck/src/main.js
+++ squadhealthcheck/src/main.js
@@ -4,9 +4,15 @@ import router from './router';
 import './assets/global.css';
 import vuetify from './plugins/vuetify';
 import { i18n } from '@/plugins/i18n';
-import { store } from './store/store'
+import { store } from './store/store';
+import VueClipboard from 'vue-clipboard2'
+
+
+VueClipboard.config.autoSetContainer = true;
 
 Vue.config.productionTip = false;
+
+Vue.use(VueClipboard);
 
 new Vue({
   router,
